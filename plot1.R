80fe75901f50ed017f69ed26bf4c28c3cb87a7de
kaungMyat-Khant <kmk.27.com@gmail.com>
2024-10-04 21:26:07 +0630

    There is error in x axis label and correct in this new script and png file

--- plot1.R
+++ plot1.R
@@ -11,6 +11,6 @@ png(filename = "plot1.png", width = 480, height = 480)
 par(mar = c(5,5,3,1))
 with(data, hist(global_active_power, 
                 col = "red1", 
-                xlab = "Global active power (kilowatts)",
+                xlab = "Global Active Power (kilowatts)",
                 main = "Global Active Power"))
 dev.off()
