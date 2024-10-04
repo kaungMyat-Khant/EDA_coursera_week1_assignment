9359718051d87afb4f613a0ded2999788a15267d
kaungMyat-Khant <kmk.27.com@gmail.com>
2024-10-04 20:39:28 +0630

    Upload script for plot 1, plot1.R

---
+++ plot1.R
@@ -0,0 +1,16 @@
+## Read data
+data <- read.csv("assignment_data.csv")
+head(data)
+
+## Missing value
+lapply(data, function(x) sum(grepl("\\?",x)))
+### No missing values marked by "?"
+
+## Plot 1 histogram
+png(filename = "plot1.png", width = 480, height = 480)
+par(mar = c(5,5,3,1))
+with(data, hist(global_active_power, 
+                col = "red1", 
+                xlab = "Global active power (kilowatts)",
+                main = "Global Active Power"))
+dev.off()
